489d9a8ff6628680b2b9c650947a3c9eecdf48bd
lio <20232685@aloe.ulima.edu.pe>
2025-11-27 09:32:38 -0500

    error en vite

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@components': '/src/components',  // alias para la carpeta de componentes
+      '@pages': '/src/pages',  // alias para la carpeta de páginas
+      '@assets': '/src/assets', // alias para la carpeta de assets
+    }
+  }
+});
