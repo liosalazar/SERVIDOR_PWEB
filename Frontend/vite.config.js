c69263366d4e774101920a8a7c8e728e15070c33
lio <20232685@aloe.ulima.edu.pe>
2025-11-27 10:49:01 -0500

    +2

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3001', // Redirige todas las solicitudes de /api al backend
+    },
+  },
   plugins: [react()],
   resolve: {
     alias: {
