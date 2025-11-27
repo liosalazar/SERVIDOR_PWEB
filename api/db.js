8ef3a41aa29c06dc54dbc7a91414a5c39e757f44
lio <20232685@aloe.ulima.edu.pe>
2025-11-27 16:57:02 -0500

    +??

--- api/db.js
+++ api/db.js
@@ -1,19 +1,14 @@
-// db.js (Usa Pool en lugar de Client)
+// db.js
 import { Pool } from 'pg';
-// Ya no necesitamos 'fs'
+import dotenv from 'dotenv'; // <--- 1. Importa dotenv
 
-// 1. Usa la variable de entorno de Azure (DATABASE_URL)
+dotenv.config(); // <--- 2. Carga las variables AQUÍ mismo
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  
-  // 2. Configuración SSL necesaria para Azure (ignora la validación)
-  // Nota: Esto reemplaza a tu ssl: false anterior, que fallaría en Azure.
   ssl: { 
     rejectUnauthorized: false 
   },
 });
 
-// 3. No es necesario llamar a pool.connect() aquí, 
-// lo haremos en index.js para probar la conexión antes de arrancar el servidor.
-
-export default pool; // Exportamos el Pool para usarlo en index.js y userRoutes.js
+export default pool;
